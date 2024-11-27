0bc4bec3ae7846bcfe1dc4e7c1c19522487508f7
CyanPC <cyan.pereira@gmail.com>
2024-11-27 18:36:18 -0300

    1st commit

---
+++ src/test/java/com/cyanpc/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyanpc.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
