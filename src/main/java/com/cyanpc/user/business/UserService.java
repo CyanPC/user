bd42fc627a65aae43bad66570a6498b7d1322d3b
CyanPC <cyan.pereira@gmail.com>
2024-12-03 11:58:38 -0300

    get delete login added

--- src/main/java/com/cyanpc/user/business/UserService.java
+++ src/main/java/com/cyanpc/user/business/UserService.java
@@ -3,8 +3,11 @@ package com.cyanpc.user.business;
 import com.cyanpc.user.business.converter.UserConverter;
 import com.cyanpc.user.business.dto.UserDTO;
 import com.cyanpc.user.infrastructure.entity.User;
+import com.cyanpc.user.infrastructure.exceptions.ConflictException;
+import com.cyanpc.user.infrastructure.exceptions.ResourceNotFoundException;
 import com.cyanpc.user.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +16,32 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserConverter userConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UserDTO saveUser(UserDTO userDTO){
+        emailExists(userDTO.getEmail());
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         User user = userConverter.toUser(userDTO);
         return userConverter.toUserDTO(userRepository.save(user));
+    }
+    public void emailExists(String email){
+        try{
+            boolean exists = verifyExistingEmail(email);
+            if(exists){
+                throw new ConflictException("Email " + email + " already registered!");
+            }
+        }catch (ConflictException e){
+            throw new ConflictException("Email already registered" + e.getCause());
+        }
+    }
+
+    public boolean verifyExistingEmail(String email){
+        return userRepository.existsByEmail(email);
+    }
+    public User findUserByEmail(String email){
+        return userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Email not found" + email));
+    }
+    public void deleteUserByEmail(String email){
+        userRepository.deleteByEmail(email);
     }
 }
--- src/main/java/com/cyanpc/user/controller/UserController.java
+++ src/main/java/com/cyanpc/user/controller/UserController.java
@@ -2,12 +2,16 @@ package com.cyanpc.user.controller;
 
 import com.cyanpc.user.business.UserService;
 import com.cyanpc.user.business.dto.UserDTO;
+import com.cyanpc.user.infrastructure.entity.User;
+import com.cyanpc.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/user")
@@ -15,9 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UserDTO> saveUser(@RequestBody UserDTO userDTO){
         return ResponseEntity.ok(userService.saveUser(userDTO));
     }
+    @PostMapping("/login")
+    public String login(@RequestBody UserDTO userDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userDTO.getEmail(),
+                        userDTO.getPassword())
+        ); //sad face
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<User> findUserByEmail(@RequestParam("email")String email){
+        return  ok(userService.findUserByEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<String> deleteUserByEmail(@PathVariable String email){
+        userService.deleteUserByEmail(email);
+        return ResponseEntity.ok("User executed.");
+    }
+
 }
+
